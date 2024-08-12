a3855736c5bda7ad41a26119340b9e26dc250f82
IMLmrchen <wxlxdxrx1456s@outlook.com>
2024-08-12 14:51:35 +0800

    Add files via upload

---
+++ scripts/oscheck.js
@@ -0,0 +1,13 @@
+var OS = 'Unknown';
+var alerts=0
+if (navigator.appVersion.indexOf('Win') != -1) OS = 'Windows';
+if (navigator.appVersion.indexOf('Mac') != -1) OS = 'MacOS';
+if (navigator.appVersion.indexOf('X11') != -1) OS = 'UNIX';
+if (navigator.appVersion.indexOf('Linux') != -1) OS = 'Linux';
+if (OS != 'Windows' && OS != 'MacOS' && OS != 'UNIX' && OS != 'Linux') {
+    alerts=1
+    }
+
+console.log(OS);
+console.log(alerts);
+
