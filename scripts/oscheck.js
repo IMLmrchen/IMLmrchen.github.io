d2cae212394aee48ed48710e17aae108ba5e2f68
IMLmrchen <wxlxdxrx1456s@outlook.com>
2024-08-12 21:09:25 +0800

    Update oscheck.js

--- scripts/oscheck.js
+++ scripts/oscheck.js
@@ -4,7 +4,7 @@ if (navigator.appVersion.indexOf('Win') != -1) OS = 'Windows';
 if (navigator.appVersion.indexOf('Mac') != -1) OS = 'MacOS';
 if (navigator.appVersion.indexOf('X11') != -1) OS = 'UNIX';
 if (navigator.appVersion.indexOf('Linux') != -1) OS = 'Linux';
-if (OS != 'Windows' && OS != 'MacOS' && OS != 'UNIX' && OS != 'Linux') {
+if (OS != 'Windows' && OS != 'MacOS'  && OS != 'Linux') {
     alerts=1
     }
 
