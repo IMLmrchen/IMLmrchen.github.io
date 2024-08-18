9201a42346c1bbcce02116af9def75ca199f365e
IMLmrchen <wxlxdxrx1456s@outlook.com>
2024-08-18 22:00:38 +0800

    Update lang.js

--- scripts/lang.js
+++ scripts/lang.js
@@ -6,15 +6,6 @@ function ci(className, innerHTML) {
   }
 }
 
-function osct() {
-
-}
-function opct() {
-
-}
-function oet() {
-
-}
 
 function scn() {
   document.querySelector('html').lang = 'zh-CN'
@@ -22,12 +13,12 @@ function scn() {
   ci('.-abtcl', '<i class="bi bi-info-square"></i>关于')
   ci('.-csmcl', '<i class="bi bi-box-arrow-up-right"></i>传送门')
   ci('.-langset', '<i class="bi bi-translate"></i>语种选择：')
-  ci('.-title', 'Mr.Chen官方网站-网站主页')
+  ci('.-title', 'Mr.Chen官方网站')
   ci('.-examimg', '示例图片')
   ci('.abttxt', '关于...')
   ci('.-memtitle', '回忆-我们的十一')
+  ci('.themeauto', '是否启用本网站的颜色自动适配功能（跟随系统主题色）？<a href="open.html" class="alert-link">启用</a>')
   osct()
-
 }
 function pcn() {
   document.querySelector('html').lang = 'zh-HT'
@@ -35,8 +26,9 @@ function pcn() {
   ci('.-abtcl', '<i class="bi bi-info-square"></i>關於')
   ci('.-csmcl', '<i class="bi bi-box-arrow-up-right"></i>傳送門')
   ci('.-langset', '<i class="bi bi-translate"></i>語種選擇：')
-  ci('.-title', 'Mr.Chen官方網站-網站主頁')
+  ci('.-title', 'Mr.Chen官方網站')
   ci('.-examimg', '示例圖片')
+  ci('.themeauto', '是否啟用本網站的顏色自動適配功能（跟隨系統主題色）？<a href="open.html" class="alert-link">啟用</a>')
   opct()
 
 }
@@ -46,8 +38,9 @@ function en() {
   ci('.-abtcl', '<i class="bi bi-info-square"></i>About')
   ci('.-csmcl', '<i class="bi bi-box-arrow-up-right"></i>Gateway')
   ci('.-langset', '<i class="bi bi-translate"></i> Language selection：')
-  ci('.-title', 'Mr.Chen official website-Homepage')
+  ci('.-title', 'Mr.Chen official website')
   ci('.-examimg', 'Example Image')
+  ci('.themeauto', 'Do you want to enable the automatic color adaptation feature of this website (following the system theme color)?<a href="open.html" class="alert-link">Enable</a>')
   oet()
 
 }
