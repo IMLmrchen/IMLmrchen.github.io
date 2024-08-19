2a5d10477efd090d193db417d10a308a92c18799
IMLmrchen <wxlxdxrx1456s@outlook.com>
2024-08-19 16:30:11 +0800

    Add files via upload

---
+++ scripts/uselesswords.js
@@ -0,0 +1,63 @@
+switch(Math.floor(Math.random() * 21)){
+    case 1:
+    $("#know").html('能力越大，能力就越大。');
+    break
+    case 2:
+    $("#know").html('情况嘛就是这么个情况，具体什么情况还得看情况。');
+    break
+    case 3:
+    $("#know").html('第一次去美国的时候惊呆了，我之前从来没有在一个国家见到这么多美国人。');
+    break
+    case 4:
+    $("#know").html('雪崩的时候，没有一片雪花是不崩的。');
+    break
+    case 5:
+    $("#know").html('卑鄙是卑鄙者的前两个字，高尚是高尚者的前两个字。');
+    break
+    case 6:
+    $("#know").html('我之前还没发现，当我发现的时候我已经发现了。');
+    break
+    case 7:
+    $("#know").html('庆历四年春，滕子京谪守巴陵郡，越明年，庆历五年。');
+    break
+    case 8:
+    $("#know").html('其实如果学习不累也挺轻松的。');
+    break
+    case 9:
+    $("#know").html('调查显示人一生只会被生下来一次。');
+    break
+    case 10:
+    $("#know").html('我曾经在极度愤怒的情况下，非常生气。');
+    break
+    case 11:
+    $("#know").html('你的语文老师是教你语文的吧。');
+    break
+    case 12:
+    $("#know").html('姜还是老的老。');
+    break
+    case 13:
+    $("#know").html('现在的年轻人啊，和老一辈比起来真的年轻。');
+    break
+    case 14:
+    $("#know").html('我前脚刚走，后脚就跟上了。');
+    break
+    case 15:
+    $("#know").html('亲测高效的防晒小技巧——避开太阳。');
+    break
+    case 16:
+    $("#know").html('我长话短说，但是说来话长。');
+    break
+    case 17:
+    $("#know").html('没有对象的人，应该还是单身吧。');
+    break
+    case 18:
+    $("#know").html('万里无云的天空中竟然没有一点云彩。');
+    break
+    case 19:
+    $("#know").html('不成功！则失败！');
+    break
+    case 20:
+    $("#know").html('七日不见，如隔一周。');
+    break
+
+  }
