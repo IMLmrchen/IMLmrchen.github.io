b1ee88cc3aea2c8fa67ff1b4a3caf09d642254b8
IMLmrchen <wxlxdxrx1456s@outlook.com>
2024-08-25 21:20:13 +0800

    Update theme.js

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,25 +1,25 @@
+var img = document.getElementById("fhlogo001")
+const html = document.querySelector('html')
 
-    const html = document.querySelector('html')
+var dl = 1
+function lightStyle() {
+    html.setAttribute('data-bs-theme', 'light')
 
-    var dl = 1
-    function lightStyle() {
-        html.setAttribute('data-bs-theme', 'light')
+}
+function darkStyle() {
+    html.setAttribute('data-bs-theme', 'dark')
+}
+const mediaQueryListDark = window.matchMedia('(prefers-color-scheme: dark)');
+if (mediaQueryListDark.matches) {
+    darkStyle()
+    dl = 0
+    img.src='https://pic.imgdb.cn/item/66c431bed9c307b7e9447ab8.png'
+}
 
-    }
-    function darkStyle() {
-        html.setAttribute('data-bs-theme', 'dark')
-
-    }
-    const mediaQueryListDark = window.matchMedia('(prefers-color-scheme: dark)');
-    if (mediaQueryListDark.matches) {
-        darkStyle()
-        dl = 0
-    }
-
-    const mediaQueryListLight = window.matchMedia('(prefers-color-scheme: light)');
-    if (mediaQueryListLight.matches) {
-        lightStyle()
-        dl = 1
-    }
-    console.log('dl=' + dl)
-
+const mediaQueryListLight = window.matchMedia('(prefers-color-scheme: light)');
+if (mediaQueryListLight.matches) {
+    lightStyle()
+    dl = 1
+    img.src='https://pic.imgdb.cn/item/66c43216d9c307b7e9452eea.png'
+}
+console.log('dl=' + dl)
